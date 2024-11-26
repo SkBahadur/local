1a9851fd51ec431de35c75a62f6c3233b9b65903
skasifbahadur <mrawesomedreamer5@gmail.com>
2024-11-26 18:59:34 +0530

    added file

---
+++ adding.java
@@ -0,0 +1,17 @@
+package localrepo;
+
+public class adding {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 10;
+        b = 20;
+        int c = a + b;
+        System.out.println(c);
+        int d;
+        d = 20;
+        System.out.println(d);
+
+    }
+
+}
